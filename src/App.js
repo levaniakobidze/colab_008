405c9795d4ffb84c918c4d2a3896a68c3d768b34
Jorjikia Beka <jorjikiabeka@Jorjikias-MacBook-Air.local>
2023-04-19 20:55:49 +0400

    router install

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import Home from "./pages/Home/Home";
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
       <h1>Hello World colab_008 </h1>
     </div>
   );
