45d6400909b342775c07b59e503c4b8c48598f63
MakoDoli <makodolidze@hotmail.com>
2023-04-27 20:26:50 +0400

    feat:add features hero

--- src/pages/Features/Features.js
+++ src/pages/Features/Features.js
@@ -1,8 +1,10 @@
 import Features6 from "../../components/FeaturesTexts/Features6";
+import FeaturesHero from "../../components/FeaturesTexts/FeaturesHero";
 
 export default function Features() {
   return (
     <div>
+      <FeaturesHero />
       <Features6 />
     </div>
   );
